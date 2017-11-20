bbfbb4f6eae5d86d89bc577fea55db82ee918ba8
David <dai@dai.me>
2017-11-20 09:31:47 -0800

    Adding .NET Framework projects due to VS's poor support for .NET Core projects.

--- src/DbfDataReader/Properties/AssemblyInfo.cs
+++ src/DbfDataReader/Properties/AssemblyInfo.cs
@@ -1,4 +1,5 @@
-﻿using System.Reflection;
+﻿using System;
+using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
@@ -17,3 +18,6 @@ using System.Runtime.InteropServices;
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("40a3f4fc-21a9-48fc-8fbb-85b9ae7c3ec1")]
+
+[assembly: CLSCompliant(isCompliant: true)]
+
