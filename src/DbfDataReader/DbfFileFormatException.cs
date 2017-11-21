0ffdc8bfb5ea6c85ac0c7b49680a2a3995ee8450
Dai <dai@dai.me>
2017-11-20 19:27:02 -0800

    DbfFileFormatException now FxCop-compliant.

--- src/DbfDataReader/DbfFileFormatException.cs
+++ src/DbfDataReader/DbfFileFormatException.cs
@@ -1,8 +1,14 @@
 using System;
+using System.Runtime.Serialization;
 
 namespace DbfDataReader
 {
+    [Serializable]
     public class DbfFileFormatException : Exception
     {
+        public DbfFileFormatException() { }
+        public DbfFileFormatException(string message) : base( message ) { }
+        public DbfFileFormatException(string message, Exception inner) : base( message, inner ) { }
+        protected DbfFileFormatException(SerializationInfo info, StreamingContext context) : base( info, context ) { }
     }
 }
