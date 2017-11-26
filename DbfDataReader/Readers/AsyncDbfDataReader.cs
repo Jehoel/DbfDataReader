6c86bc8508b97524ff18819b9fdea3535fe173f6
David <dai@dai.me>
2017-11-26 04:00:47 -0800

    Moving DbfDataReader classes into their own files and directory.

---
+++ DbfDataReader/Readers/AsyncDbfDataReader.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.IO;
+using System.Security.AccessControl;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Overby.Extensions.AsyncBinaryReaderWriter;
+
+namespace DbfDataReader
+{
+    public sealed class AsyncDbfDataReader : DbfDataReader
+    {
+        private readonly FileStream        fileStream;
+        private readonly AsyncBinaryReader binaryReader;
+        private          Boolean           isDisposed;
+        private          Boolean           isEof;
+
+        private readonly DbfDataReaderOptions options;
+
+        public override Encoding Encoding { get; }
+
+        /// <param name="ignoreEof">If true, then a </param>
+        internal AsyncDbfDataReader(DbfTable table, String fileName, Boolean randomAccess, Encoding encoding, DbfDataReaderOptions options)
+            : base( table )
+        {
+            FileOptions fileOptions = FileOptions.Asynchronous | ( randomAccess ? FileOptions.RandomAccess : FileOptions.SequentialScan );
+
+            FileStream stream = new FileStream( fileName, FileMode.Open, FileSystemRights.ReadData, FileShare.ReadWrite, 4096, fileOptions );
+            if( !stream.CanRead || !stream.CanSeek )
+            {
+                stream.Dispose();
+                throw new InvalidOperationException("The created FileStream could not perform both Read and Seek operations.");
+            }
+            if( !stream.IsAsync )
+            {
+                stream.Dispose();
+                throw new InvalidOperationException("The created FileStream is not asynchronous.");
+            }
+
+            this.binaryReader = new AsyncBinaryReader( this.fileStream, Encoding.ASCII, leaveOpen: true );
+
+            this.Encoding = encoding;
+
+            this.options = options;
+        }
+
+        public override void Close()
+        {
+            this.binaryReader.Dispose();
+            this.fileStream.Dispose();
+            this.isDisposed = true;
+        }
+
+        public override Boolean IsClosed => this.isDisposed;
+
+        protected override Boolean EOF => this.isEof;
+
+        private Boolean SetEOF()
+        {
+            if( this.EOF ) return true;
+
+            if( this.binaryReader.BaseStream.Position == this.binaryReader.BaseStream.Length )
+            {
+                this.isEof = true;
+            }
+
+            return this.EOF;
+        }
+
+        public override Boolean Read()
+        {
+            throw new NotSupportedException("Synchronous reading is not supported.");
+        }
+
+        public override Task<Boolean> ReadAsync(CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException("TODO!");
+        }
+    }
+}
---
+++ DbfDataReader/Readers/DbfDataReader.cs
@@ -0,0 +1,188 @@
+﻿using System;
+using System.Collections;
+using System.Data.Common;
+using System.Text;
+
+namespace DbfDataReader
+{
+    public abstract class DbfDataReader : DbDataReader
+    {
+        public DbfTable Table { get; }
+
+        internal DbfDataReader(DbfTable table)
+        {
+            this.Table = table;
+        }
+
+        private DbfRecord current;
+
+        public DbfRecord Current
+        {
+            get
+            {
+                if( this.current == null ) throw new InvalidOperationException("No " + nameof(DbfRecord) + " has been read by this " + nameof(DbfDataReader) + " instance.");
+                return this.current;
+            }
+            protected set
+            {
+                this.current = value;
+            }
+        }
+
+        public override abstract void Close();
+
+        #region DbDataReader
+
+        #region IDataRecord / Get typed values
+
+        public override Boolean GetBoolean(Int32 ordinal)
+        {
+            return this.Current.GetBoolean( ordinal );
+        }
+
+        public override Byte GetByte(Int32 ordinal)
+        {
+           return this.Current.GetByte( ordinal );
+        }
+
+        public override Int64 GetBytes(Int32 ordinal, Int64 dataOffset, Byte[] buffer, Int32 bufferOffset, Int32 length)
+        {
+            return this.Current.GetBytes( ordinal, dataOffset, buffer, bufferOffset, length );
+        }
+
+        public override Char GetChar(Int32 ordinal)
+        {
+            return this.Current.GetChar( ordinal );
+        }
+
+        public override Int64 GetChars(Int32 ordinal, Int64 dataOffset, Char[] buffer, Int32 bufferOffset, Int32 length)
+        {
+           return this.Current.GetChars( ordinal, dataOffset, buffer, bufferOffset, length );
+        }
+
+        public override DateTime GetDateTime(Int32 ordinal)
+        {
+            return this.Current.GetDateTime( ordinal );
+        }
+
+        public override Decimal GetDecimal(Int32 ordinal)
+        {
+            return this.Current.GetDecimal( ordinal );
+        }
+
+        public override Double GetDouble(Int32 ordinal)
+        {
+            return this.Current.GetDouble( ordinal );
+        }
+
+        public override Single GetFloat(Int32 ordinal)
+        {
+            return this.Current.GetFloat( ordinal );
+        }
+
+        public override Guid GetGuid(Int32 ordinal)
+        {
+            return this.Current.GetGuid( ordinal );
+        }
+
+        public override Int16 GetInt16(Int32 ordinal)
+        {
+            return this.Current.GetInt16( ordinal );
+        }
+
+        public override Int32 GetInt32(Int32 ordinal)
+        {
+            return this.Current.GetInt32( ordinal );
+        }
+
+        public override Int64 GetInt64(Int32 ordinal)
+        {
+            return this.Current.GetInt64( ordinal );
+        }
+
+        public override String GetString(Int32 ordinal)
+        {
+            return this.Current.GetString( ordinal );
+        }
+
+        #endregion
+
+        public override Int32 Depth => 0; // always zero depth.
+
+        public override Int32 FieldCount => this.Current.FieldCount;
+
+        /// <summary>Returns false if the current table file contains no records or if this reader has reached EOF.</summary>
+        public override Boolean HasRows
+        {
+            get
+            {
+                if( this.IsClosed ) throw new InvalidOperationException("This " + nameof(DbfDataReader) + " is closed.");
+
+                if( this.Table.Header.RecordCount == 0 ) return false;
+                
+                if( this.EOF ) return false;
+
+                return true;
+            }
+        }
+
+        public override Int32 RecordsAffected => 0; // we're read-only
+
+        public override Object this[String name] => this.Current[ name ];
+
+        public override Object this[Int32 ordinal] => this.Current[ ordinal ];
+
+        public override String GetDataTypeName(Int32 ordinal) => this.Current.GetDataTypeName( ordinal );
+
+        public override IEnumerator GetEnumerator()
+        {
+            return new DbEnumerator( this, closeReader: false );
+        }
+
+        public override Type GetFieldType(Int32 ordinal) => this.Current.GetFieldType( ordinal );
+
+        public override String GetName(Int32 ordinal) => this.Current.GetName( ordinal );
+
+        public override Int32 GetOrdinal(String name) => this.Current.GetOrdinal( name );
+
+        public override Object GetValue(Int32 ordinal) => this.Current.GetValue( ordinal );
+
+        public override Int32 GetValues(Object[] values) => this.Current.GetValues( values );
+
+        public override Boolean IsDBNull(Int32 ordinal) => this.Current.IsDBNull( ordinal );
+
+        public override Boolean NextResult()
+        {
+            // xBase does not have a concept of batched results.
+            return false;
+        }
+
+        #endregion
+
+        protected abstract Boolean EOF { get; }
+
+        public abstract Encoding Encoding { get; }
+
+        // DbDataReader already has a `public virtual Task<Boolean> ReadAsync(CancellatioinToken)` method that calls `this.Read()` synchronously.
+        // So `SyncDbfDataReader` doesn't need to do anything.
+
+        protected enum DbfReadResult
+        {
+            Read,
+            Eof,
+            Skipped
+        }
+    }
+
+    [Flags]
+    public enum DbfDataReaderOptions
+    {
+        None          = 0,
+        /// <summary>DBF EOF values will be ignored and the entire DBF file will be read.</summary>
+        IgnoreEof     = 1 << 0,
+        /// <summary>Records with DbfRecordStatus.Deleted values will be read instead of being skipped-over.</summary>
+        AllowDeleted = 1 << 1,
+        /// <summary>Records with DbfRecordStatus values other than Deleted or Valid will be read instead of being skipped-over.</summary>
+        AllowInvalid  = 1 << 2
+    }
+}
