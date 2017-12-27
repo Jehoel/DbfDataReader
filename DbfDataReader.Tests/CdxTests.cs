090efcd70db54603587ffe46cecef55ae1545022
Dai <dai@dai.me>
2017-12-27 16:19:49 +0000

    Adding expected index-tags to test.

--- DbfDataReader.Tests/CdxTests.cs
+++ DbfDataReader.Tests/CdxTests.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 
@@ -36,7 +37,31 @@ namespace DbfDataReader.NetFx.Tests
                 keyInfo.KeyValue = keyValue;
             }
 
+            Tuple<String,Int32>[] expectedIndexTags = new Tuple<String,Int32>[]
+            {
+                Tuple.Create("APPBAL", 3956224 ), 
+                Tuple.Create("APPCODE", 3952128 ), 
+                Tuple.Create("APPNAME", 3929600 ), 
+                Tuple.Create("APPROVED", 1224192 ), 
+                Tuple.Create("BALANCE", 1250304 ), 
+                Tuple.Create("CODE", 76288 ), 
+                Tuple.Create("EMAIL", 3596800 ), 
+                Tuple.Create("F_PHONE", 962560 ), 
+                Tuple.Create("GLPOST", 2051584 ), 
+                Tuple.Create("H_PHONE", 743936 ), 
+                Tuple.Create("KEY", 1536 ), 
+                Tuple.Create("M_PHONE", 1042944 ), 
+                Tuple.Create("NAME", 134144 ), 
+                Tuple.Create("NAMEBAL", 1246720 ), 
+                Tuple.Create("NAMEIA", 2851328 ), 
+                Tuple.Create("SM_KEY", 1251840 ), 
+                Tuple.Create("SRCDATE", 2865664 ), 
+                Tuple.Create("STORENAME", 2960896 ), 
+                Tuple.Create("W_PHONE", 864256 )
+            };
 
+            Assert.Equal( expectedIndexTags.Select( t => t.Item1 ), list.Select( e => e.KeyValue ) );
+            Assert.Equal( expectedIndexTags.Select( t => t.Item1 ), list.Select( e => e.KeyValue ) );
 
             String x = "foo";
         }
