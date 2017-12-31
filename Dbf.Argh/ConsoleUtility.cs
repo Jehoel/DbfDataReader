29ce86120ba7b7debd0de599a15917c608bbaba9
Dai <dai@dai.me>
2017-12-31 19:44:05 +0000

    Adding an interactive test program "Dbf.Argh".

---
+++ Dbf.Argh/ConsoleUtility.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dbf.Argh
+{
+	public static class ConsoleUtility
+	{
+		public static void PrintArray(IList<Object[]> table)
+		{
+			Int32[] colWidths = GetColumnWidths( table );
+			String lineFormat = GetLineFormat( colWidths );
+
+			///////////////////////////////////
+
+			Boolean first = true;
+			foreach( Object[] row in table )
+			{
+				for( Int32 c = 0; c < row.Length; c++ )
+				{
+					if( row[c] is IConvertible convVal && !(row[c] is String) )
+					{
+						row[c] = convVal.ToString( CultureInfo.CurrentCulture ).PadLeft( colWidths[c] );
+					}
+				}
+
+				Console.WriteLine( lineFormat, arg: row );
+
+				if( first )
+				{
+					Int32 totalWidth = ( ( colWidths.Length - 1 ) * 3 ) + colWidths.Sum();
+					Console.WriteLine( "".PadRight( totalWidth, '-' ) );
+					first = false;
+				}
+			}
+
+			Console.WriteLine();
+		}
+
+		private static Int32[] GetColumnWidths(IList<Object[]> table)
+		{
+			Int32[] colWidths = new Int32[ table.First().Length ];
+			foreach( Object[] row in table )
+			{
+				for( Int32 c = 0; c < row.Length; c++ )
+				{
+					String stringValue = row[c]?.ToString() ?? String.Empty;
+
+					Int32 l = stringValue.Length;
+					if( l > colWidths[c] ) colWidths[c] = l;
+				}
+			}
+			return colWidths;
+		}
+
+		private static String GetLineFormat(Int32[] colWidths)
+		{
+			StringBuilder sb = new StringBuilder( 1024 );
+			for( Int32 i = 0; i < colWidths.Length; i++ )
+			{
+				if( i > 0 ) sb.Append(" | ");
+
+				sb.Append("{");
+				sb.AppendFormat("{0},{1}", i, -colWidths[i] );
+				sb.Append("}");
+			}
+
+			String lineFormat = sb.ToString();
+			return lineFormat;
+		}
+
+		public static UInt32 ReadUInt32(String prompt)
+		{
+			do
+			{
+				Console.ForegroundColor = ConsoleColor.White;
+				Console.WriteLine( prompt );
+				Console.ResetColor();
+
+				Console.ForegroundColor = ConsoleColor.Cyan;
+				String input = Console.ReadLine();
+				Console.ResetColor();
+
+				UInt32 output;
+				if( UInt32.TryParse( input, NumberStyles.Integer, CultureInfo.CurrentCulture, out output ) ) return output;
+
+				Console.ForegroundColor = ConsoleColor.Yellow;
+				Console.WriteLine( "Could not parse \"{0}\" as UInt32.", input );
+				Console.ResetColor();
+
+			} while( true );
+		}
+
+		public static String ReadLine(String prompt)
+		{
+			do
+			{
+				Console.ForegroundColor = ConsoleColor.White;
+				Console.WriteLine( prompt );
+				Console.ResetColor();
+
+				Console.ForegroundColor = ConsoleColor.Cyan;
+				String input = Console.ReadLine();
+				Console.ResetColor();
+
+				if( !String.IsNullOrWhiteSpace( input ) ) return input;
+
+				Console.ForegroundColor = ConsoleColor.Yellow;
+				Console.WriteLine("Empty input.");
+				Console.ResetColor();
+
+			} while( true );
+		}
+	}
+}
---
+++ Dbf.Argh/Program.cs
@@ -0,0 +1,176 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Dbf.Cdx;
+
+namespace Dbf.Argh
+{
+	public class Program
+	{
+		public static void Main(string[] args)
+		{
+			//DumpIndex( @"C:\git\cdx\DBD-XBase\t\rooms.cdx" );
+
+			//DumpIndex( @"C:\git\rss\DbfDataReader\DbfDataReader\DbfDataReader.Tests\TestData\foxprodb\calls.CDX" );
+
+			//DumpIndex( @"C:\git\rss\DbfDataReader\Data\CUSTOMER.CDX" );
+
+			DumpIndex( @"C:\git\rss\DbfDataReader\Data\CUSTOMER-dbfMan-1.cdx" );
+		}
+
+		public static void DumpIndex(String fileName)
+		{
+			CdxFile indexFile = CdxFile.Open( fileName );
+			ExteriorCdxNode rootNode = (ExteriorCdxNode)indexFile.RootNode;
+
+			///////////////////////////////////////
+
+			var indexes = rootNode
+				.IndexKeys
+				.Select( ik => new { Key = ik, RootNode = indexFile.ReadCompactIndex( ik.RecordNumber ) } );
+
+			Dictionary<Int64,BaseCdxNode> rootNodeHeaders = indexes.ToDictionary( pair => pair.RootNode.Offset, pair => pair.RootNode );
+
+			{
+				List<Object[]> output = new List<Object[]>();
+				output.Add( _indexHeaders );
+				output.AddRange( indexes.Select( pair => DumpIndex( indexFile, pair.Key, pair.RootNode ) ) );
+				ConsoleUtility.PrintArray( output );
+			}
+			
+			BaseCdxNode parentNode = null;
+
+			while( true )
+			{
+				UInt32 nodeOffset = ConsoleUtility.ReadUInt32( "Open node at offset? Or 0 to quit. Do not specify an Index Header offset.\r\nOffset must be a child of the current node." );
+				if( nodeOffset == 0 ) return;
+
+				BaseCdxNode node;
+
+				if( rootNodeHeaders.ContainsKey( nodeOffset ) )
+				{
+					parentNode = rootNodeHeaders[ nodeOffset ];
+
+					Console.WriteLine("Specified node is a root node. Reading inner compact-index.");
+					node = indexFile.ReadNode( parentNode, nodeOffset );
+				}
+				else
+				{
+					if( parentNode == null )
+					{
+						Console.WriteLine("Error. No current parent node.");
+						return;
+					}
+
+					Console.WriteLine("Specified node is not a root node. Reading node directly.");
+					node = indexFile.ReadNode( parentNode, nodeOffset );
+				}
+				
+				parentNode = node;
+
+				{
+					List<Object[]> output = new List<Object[]>();
+					output.Add( _nodeHeaders );
+				
+					if( node is InteriorCdxNode intNode )
+					{
+						output.Add( new Object[] { "Interior", intNode.Attributes, intNode.LeftSibling, intNode.RightSibling, intNode.KeyCount, intNode.KeyValues } );
+					}
+					else if( node is ExteriorCdxNode extNode )
+					{
+						output.Add( new Object[] { "Exterior", extNode.Attributes, extNode.LeftSibling, extNode.RightSibling, extNode.KeyCount, "" } );
+					}
+
+					ConsoleUtility.PrintArray( output );
+				}
+
+				{
+					if( node is InteriorCdxNode intNode )
+					{
+						Console.WriteLine("Interior node keys:");
+						Console.WriteLine();
+
+						List<Object[]> output = new List<Object[]>();
+						output.Add( _interiorKeyHeaders );
+						output.AddRange( intNode.KeyEntries.Select( ke => DumpInteriorKeyEntry( ke ) ) );
+						ConsoleUtility.PrintArray( output );
+					}
+					else if( node is ExteriorCdxNode extNode )
+					{
+						Console.WriteLine("Exterior node keys:");
+						Console.WriteLine();
+
+						List<Object[]> output = new List<Object[]>();
+						output.Add( _exteriorKeyHeaders );
+						output.AddRange( extNode.IndexKeys.Select( ke => DumpExteriorKeyEntry( ke ) ) );
+						ConsoleUtility.PrintArray( output );
+					}
+				}
+			}
+
+			Console.ReadLine();
+		}
+
+		private static Object[] _indexHeaders = new Object[] { "Index file", "Tag", "Header offset", "Root node offset", "Expression", "Order", "Unique", "Root type", "Key length", "Filter" };
+
+		private static Object[] DumpIndex(CdxFile indexFile, CdxKeyEntry key, BaseCdxNode rootNode)
+		{
+			CdxFileHeader h = rootNode.IndexHeader;
+
+			String file = indexFile.FileInfo.Name;
+			String tag  = key.StringKey;
+			Object hoff = h.Offset;
+			Object roff = rootNode.Offset;
+			String expr = Encoding.ASCII.GetString( h.KeyExpressionPool, 0, h.KeyExpressionPoolLength );
+			String ordr = h.Order.ToString();
+			String uniq = h.Options.HasFlag( CompactIndexOptions.Unique ) ? "True" : "False";
+			String type = rootNode is ExteriorCdxNode ? "Exterior" : "Interior";
+			Object klen = h.KeyLength;
+			String filt = h.Options.HasFlag( CompactIndexOptions.HasForClause ) ?
+				Encoding.ASCII.GetString( h.KeyExpressionPool, h.KeyExpressionPoolLength, h.ForExpressionPoolLength ) :
+				String.Empty;
+
+			return new Object[] { file, tag, hoff, roff, expr, ordr, uniq, type, klen, filt };
+		}
+
+		private static readonly Object[] _nodeHeaders = new Object[] { "Type", "Attributes", "Left", "Right", "KeyCount", "KeyValue" };
+
+		private static readonly Object[] _interiorKeyHeaders = new Object[] { "Key ASCII", "Key bytes", "Recno", "nPage" };
+
+		private static Object[] DumpInteriorKeyEntry(InteriorIndexKeyEntry entry)
+		{
+			String bytesAscii = Encoding.ASCII.GetString( entry.KeyBytes );
+			String bytesHex = BitConverter.ToString( entry.KeyBytes ); // returns dash-separated uppercase hex, e.g.: "00-AA-BB-FE"
+
+			return new Object[] { bytesAscii, bytesHex, entry.RecordNumber, entry.NPage };
+		}
+
+		private static readonly Object[] _exteriorKeyHeaders = new Object[] { "Key ASCII", "Key bytes", "Number" };
+
+		private static Object[] DumpExteriorKeyEntry(CdxKeyEntry entry)
+		{
+			String keyAscii = SafeString( entry.StringKey );
+			String keyBytes = BitConverter.ToString( entry.KeyBytes );
+
+			return new Object[] { keyAscii, keyBytes, entry.RecordNumber };
+		}
+
+		private static String SafeString(String value)
+		{
+			if( value.All( c => !Char.IsControl( c ) ) ) return value;
+
+			StringBuilder sb = new StringBuilder( value.Length );
+			foreach( Char c in value )
+			{
+				if( Char.IsControl( c ) ) sb.Append( '_' );
+				else sb.Append( c );
+			}
+
+			String safe = sb.ToString();
+			return safe;
+		}
+	}
+}
